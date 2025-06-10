4638027cec128b1dbd36037f8fdfa2fee693df5d
S M Yogeesh <152467082+yogeeshsm@users.noreply.github.com>
2025-06-10 18:31:44 +0530

    Update Login Screen Application.dart

--- Login Screen Application.dart
+++ Login Screen Application.dart
@@ -49,3 +49,32 @@ class LoginScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+#Alternative 
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: LoginScreen()));
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("LoginScreen")),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(decoration: InputDecoration(labelText: "email")),
+            TextField(decoration: InputDecoration(labelText: "password")),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: Text("login")),
+          ],
+        ),
+      ),
+    );
+  }
+}
