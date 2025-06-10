f35a042e55e05345be90195b15db19fb697b41e3
S M Yogeesh <152467082+yogeeshsm@users.noreply.github.com>
2025-06-10 15:57:13 +0530

    Create E-Commerce application.dart

---
+++ E-Commerce application.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: EcommerceApp()));
+
+class EcommerceApp extends StatelessWidget {
+  final List<Map<String, String>> products = [
+    {'name': 'T-Shirt', 'price': '\$20'},
+    {'name': 'Shoes', 'price': '\$50'},
+    {'name': 'Watch', 'price': '\$80'},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Simple E-Commerce')),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return Card(
+            margin: EdgeInsets.all(8),
+            child: ListTile(
+              title: Text(product['name']!),
+              subtitle: Text(product['price']!),
+              trailing: ElevatedButton(
+                child: Text('Add to Cart'),
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('${product['name']} added to cart')),
+                  );
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
